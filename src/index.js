5db509e55fc884e2e3148cf40dba18fccc49be0a
Alex <alex.nielsen@gmail.com>
2018-03-09 21:42:28 -0500

    through lesson 21 but need to review the props lessons

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// COMPONENTS
+import Header from './components/header';
+import JSON from './db.json';
+import NewsList from './components/news_list';
+
+
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+ 
+        this.state = {
+            news: JSON,
+            filtered: JSON,
+        }
+    }
+
+filterNews(keywords) {
+    console.log(keywords)
+}
+
+    render () {
+        return (
+            <div>
+                <Header newsSearch={keywords=>this.filterNews(keywords)}/>
+                <NewsList news = {this.state.filtered}/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root') );
